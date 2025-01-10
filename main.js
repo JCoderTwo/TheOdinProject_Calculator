e3bbccab9b0baffa38a646c8582e369c1aebe832
JCoderTwo <136948116+JCoderTwo@users.noreply.github.com>
2025-01-09 22:32:28 -1000

    Add basic addition, subtraction, multiplication, and division functions.

---
+++ main.js
@@ -0,0 +1,19 @@
+function add (num1, num2) {
+    return num1 + num2
+}
+console.log (add(2,4))
+
+function subtract (num1, num2) {
+    return num1 - num2
+}
+console.log (subtract(4,1))
+
+function multiply (num1, num2) {
+    return num1 * num2
+}
+console.log(multiply(3,3))
+
+function divide (num1, num2) {
+    return num1 / num2
+}
+console.log(divide(5,5))
